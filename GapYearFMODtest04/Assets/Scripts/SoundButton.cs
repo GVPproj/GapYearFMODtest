e938c580c1690cf3d1d0b9d0c790f00476e03d19
GVPproj <grahamvanpelt@gmail.com>
2020-11-13 10:18:52 -0800

    IncorportateFirstFMODsound

---
+++ GapYearFMODtest04/Assets/Scripts/SoundButton.cs
@@ -0,0 +1,29 @@
+﻿// To use this example, attach this script to an empty GameObject.
+// Create three buttons (Create>UI>Button). Next, select your
+// empty GameObject in the Hierarchy and click and drag each of your
+// Buttons from the Hierarchy to the Your First Button, Your Second Button
+// and Your Third Button fields in the Inspector.
+// Click each Button in Play Mode to output their message to the console.
+// Note that click means press down and then release.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    public Button m_Sound01;
+
+    void Start()
+    {
+        m_Sound01.onClick.AddListener(TaskOnClick);     
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("Sound 01!");
+        FMODUnity.RuntimeManager.PlayOneShot("event:/AirportEvents/AirportAmbience");
+    }
+
+}
+
+
